3a3a6d30021f1c7f83ed847721810c7766630d80
Reyvi Rahman <reyvirahman@gmail.com>
2024-08-14 10:57:28 +0700

    Layouting first page

---
+++ app/src/main/java/com/rey/palindromecheck/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.rey.palindromecheck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rey.palindromecheck.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
